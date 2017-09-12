6e10a0775f73223a894f0676bfc471c1b2c9bbdd
TCezarRod <thalles.cezar@gmail.com>
2017-09-12 01:45:06 -0300

    Adding testRunner

---
+++ src/test/application/TestsRunner.java
@@ -0,0 +1,39 @@
+package application;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import presentation.InputParserTest;
+
+public class TestsRunner {
+
+	public static void main(String[] args) {
+
+		System.out.println("Testing");
+		Result result = JUnitCore.runClasses(InputParserTest.class);
+		
+		for(Failure fail : result.getFailures()){
+			System.out.println(fail.toString());
+		}
+		
+		System.out.println(result.wasSuccessful());
+	}
+	
+	private void runInputParserTests(){
+		
+	}
+	
+	private void runCommandTests(){
+		
+	}
+	
+	private void runWebParserTests(){
+		
+	}
+	
+	private void runLogInterpreterTests(){
+		
+	}
+	
+}
